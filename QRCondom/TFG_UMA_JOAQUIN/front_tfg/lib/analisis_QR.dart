2209ea53a820e1a20667e2bb3b09f1462ec07803
jkingxpf <124096699+jkingxpf@users.noreply.github.com>
2025-06-17 13:01:23 +0200

    Ultimo commit

---
+++ QRCondom/TFG_UMA_JOAQUIN/front_tfg/lib/analisis_QR.dart
@@ -0,0 +1,189 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:front_tfg/pantalla_rfb.dart';
+import 'package:http/http.dart' as http;
+import 'package:android_id/android_id.dart';
+
+class AnalisisQr extends StatefulWidget {
+  final String analisis;
+  final String qr;
+
+  const AnalisisQr({super.key, required this.analisis, required this.qr});
+
+  @override
+  _AnalisisQrState createState() => _AnalisisQrState();
+}
+
+class _AnalisisQrState extends State<AnalisisQr> {
+  List<Map<String, String>> analizadores = [];
+
+  @override
+  void initState() {
+    super.initState();
+    procesarAnalisis();
+  }
+
+  void procesarAnalisis() {
+    try {
+      final List<dynamic> datosJson = jsonDecode(widget.analisis);
+
+      setState(() {
+        analizadores =
+            datosJson.map<Map<String, String>>((item) {
+              return {
+                "analizador": item["analizador"].toString(),
+                "resultado": item["resultado"].toString(),
+              };
+            }).toList();
+      });
+    } catch (e) {
+      print("Error al decodificar los datos: $e");
+    }
+  }
+
+  void consultaSegura() async {
+    var url = Uri.parse('http://192.168.1.46:80/consulta_segura');
+
+    final androidIdPlugin = AndroidId();
+    final androidId = await androidIdPlugin.getId();
+
+    var body = json.encode({'android_id': androidId, 'qr_code': widget.qr});
+
+    try {
+      var response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (response.statusCode == 201) {
+        int puerto = json.decode(response.body);
+
+        print(puerto);
+
+        setState(() {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PantallaRfb(port: puerto)),
+          );
+        });
+      } else {
+        print('Error wacho: ${response.body}');
+      }
+    } catch (e) {
+      print('Request failed: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE3F2FD), 
+      appBar: AppBar(
+        title: const Text(
+          "Resultado de Análisis",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF1976D2), 
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body:
+          analizadores.isEmpty
+              ? const Center(
+                child: Text(
+                  "No hay datos",
+                  style: TextStyle(fontSize: 18, color: Colors.black54),
+                ),
+              )
+              : Column(
+                children: [
+                  Card(
+                    color: Colors.white,
+                    margin: const EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 6,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            'Contenido del QR:',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          const SizedBox(height: 12),
+                          Text(
+                            widget.qr,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: analizadores.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 6,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          elevation: 2,
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.analytics,
+                              color: ((analizadores[index]["resultado"] ?? "") == "No peligroso") ? Colors.green : Colors.red,
+                            ),
+                            title: Text(
+                              analizadores[index]["analizador"] ?? "Desconocido",
+                              style: const TextStyle(color: Colors.black87),
+                            ),
+                            subtitle: Text(
+                              analizadores[index]["resultado"] ?? "Sin resultados",
+                              style: const TextStyle(color: Colors.black54),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF1976D2),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                        ),
+                        onPressed: () => consultaSegura(),
+                        child: const Text(
+                          'Consulta segura',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+    );
+  }
+}
---
+++ QRCondom/TFG_UMA_JOAQUIN/front_tfg/lib/pantalla_rfb.dart
@@ -0,0 +1,175 @@
+import 'package:flutter_rfb/flutter_rfb.dart';
+import 'package:flutter/material.dart';
+import 'package:android_id/android_id.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class PantallaRfb extends StatefulWidget {
+  final int port;
+
+  const PantallaRfb({super.key, required this.port});
+
+  @override
+  _PantallaRfb createState() => _PantallaRfb();
+}
+
+class _PantallaRfb extends State<PantallaRfb> {
+  int segundos = 20;
+  bool cargado = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _temporizador();
+  }
+
+  void _temporizador() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (segundos > 1) {
+        setState(() {
+          segundos--;
+        });
+      } else {
+        timer.cancel();
+        setState(() {
+          cargado = true;
+        });
+      }
+    });
+  }
+
+  void cerrarSesion(BuildContext context) async {
+    final androidIdPlugin = AndroidId();
+    final androidId = await androidIdPlugin.getId();
+
+    var url = Uri.parse('http://192.168.1.46:80/cerrar_sesion_consulta_segura');
+
+    var body = json.encode({'android_id': androidId});
+
+    try {
+      var response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+      } else {
+        print('Error wacho: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error try: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFE3F2FD),
+        appBar: AppBar(
+          title: Text('Consulta Segura', style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color(0xFF1565C0),
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue.withOpacity(0.1),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child:
+                      cargado
+                          ? UsuarioVNC(widget.port, '192.168.1.46')
+                          : TextoCargando(segundos),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FloatingActionButton(
+                  backgroundColor: const Color(0xFF0D47A1), // Azul oscuro
+                  onPressed: () => cerrarSesion(context),
+                  child: const Icon(Icons.close, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UsuarioVNC extends StatelessWidget {
+  final int puerto;
+  final String ip;
+
+  UsuarioVNC(this.puerto, this.ip);
+
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: InteractiveViewer(
+        constrained: true,
+        maxScale: 10,
+        child: RemoteFrameBufferWidget(
+          hostName: ip,
+          port: 5900 + puerto,
+          onError: (Object error) {
+            print(puerto);
+            print('Error de conexión: $error');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TextoCargando extends StatelessWidget {
+  int segundos;
+
+  TextoCargando(this.segundos);
+
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Spacer(),
+        const SizedBox(
+          width: 24,
+          height: 24,
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+            valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF1565C0)),
+          ),
+        ),
+        const SizedBox(width: 16),
+        Text(
+          "Cargando... espera $segundos segundos",
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF0D47A1),
+          ),
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+}
